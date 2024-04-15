a7bb24db102b0fbe2fe190ae21eb66fd0c189e25
Pingyoping <64616729+Pingyoping@users.noreply.github.com>
2024-04-15 13:16:49 -0400

    Finished CodeBreaker
    We done it boys, the code is broken.
    
    Solves the code in an average of 5-6 guesses.

--- AB_game/AB_game/BreakCode.cs
+++ AB_game/AB_game/BreakCode.cs
@@ -7,100 +7,82 @@ namespace AB_game
 {
     public partial class BreakCode : Form
     {
-        private string code;
+        private string currentGuess;
         private welcome form;
-        private List<string> possibleGuesses = GenerateAllPossibleGuesses();
-        private List<string> previousGuesses = new List<string>();
+        private List<string> possibleNumbers;
+        private int guessCount;
 
         public BreakCode(string code, welcome form)
         {
             InitializeComponent();
-            this.code = code;
+            InitializeGame();
+            // Start the game with the first guess
             this.form = form;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string hint = textBox_Hint.Text;
+            int cows = int.Parse(hint.Substring(0, 1));
+            int bulls = int.Parse(hint.Substring(2, 1));
+
+            // Remove impossible numbers based on the hint
+            possibleNumbers = possibleNumbers.Where(num =>
+            {
+                int numCows = CountCows(num, currentGuess);
+                int numBulls = CountBulls(num, currentGuess);
+                return numCows == cows && numBulls == bulls;
+            }).ToList();
+
+            if (cows == 4) // If all cows, the game is won
+            {
+                label_Error.Text = $"Congratulations! The secret number was {currentGuess}. Guessed in {guessCount} attempts.";
+                InitializeGame(); // Start a new game
+            }
+            else
+            {
+                // Move to the next guess
+                currentGuess = possibleNumbers.First();
+                guessCount++;
+                textBox_Guess.Text = currentGuess;
+            }
+        }
+
+        private void InitializeGame()
+        {
+            possibleNumbers = GeneratePossibleNumbers();
+            currentGuess = possibleNumbers.First(); // Start with the first guess
+            guessCount = 1;
+            textBox_Guess.Text = currentGuess;
+        }
+
+        private List<string> GeneratePossibleNumbers()
+        {
+            return Enumerable.Range(0, 10000)
+                .Select(i => i.ToString("D4"))
+                .Where(num => num.Distinct().Count() == 4)
+                .ToList();
+        }
+
+        private int CountCows(string secret, string guess)
+        {
+            return secret.Where((t, i) => t == guess[i]).Count();
+        }
+
+        private int CountBulls(string secret, string guess)
+        {
+            return guess.Count(t => secret.Contains(t)) - CountCows(secret, guess);
+        }
+
+        private void BreakCode_Load(object sender, EventArgs e)
+        {
+            textBox_Guess.Text = currentGuess;
         }
 
         private void BreakCode_FormClosed(object sender, FormClosedEventArgs e)
         {
             form.Show();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string hint = textBox_Hint.Text.Trim();
-            if (!string.IsNullOrEmpty(hint))
-            {
-                textBox_Guess.Text = MakeGuess(hint);
-            }
-            else
-            {
-                label_Error.Text = "Please provide a hint before making a guess.";
-            }
-        }
-
-        private string MakeGuess(string hint)
-        {
-            // Filter out guesses that match the hint
-            List<string> filteredGuesses = possibleGuesses.Where(guess => IsConsistentWithHint(guess, hint)).ToList();
-
-            // Avoid making the same guess twice
-            filteredGuesses = filteredGuesses.Except(previousGuesses).ToList();
-
-            // Select the next guess
-            string nextGuess = filteredGuesses.FirstOrDefault();
-            previousGuesses.Add(nextGuess);
-
-            return nextGuess;
-        }
-
-        private bool IsConsistentWithHint(string guess, string hint)
-        {
-            int correctPositionCount = 0;
-            int correctDigitCount = 0;
-
-            for (int i = 0; i < guess.Length; i++)
-            {
-                if (guess[i] == hint[i])
-                    correctPositionCount++;
-                else if (hint.Contains(guess[i]))
-                    correctDigitCount++;
-            }
-
-            return $"{correctPositionCount}A{correctDigitCount}B" == hint;
-        }
-
-        private static List<string> GenerateAllPossibleGuesses()
-        {
-            List<string> guesses = new List<string>();
-
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    if (j == i)
-                        continue;
-
-                    for (int k = 0; k < 10; k++)
-                    {
-                        if (k == i || k == j)
-                            continue;
-
-                        for (int l = 0; l < 10; l++)
-                        {
-                            if (l == i || l == j || l == k)
-                                continue;
-
-                            guesses.Add($"{i}{j}{k}{l}");
-                        }
-                    }
-                }
-            }
-
-            return guesses;
-        }
-
-        private void BreakCode_Load(object sender, EventArgs e)
-        {
-            textBox_Guess.Text = code;
         }
     }
 }
